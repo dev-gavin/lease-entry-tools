920d4bd41c291fb4ca249dd05422ffd01783d6ed
Gavin Meeker <gmeeker99@gmail.com>
2023-07-29 15:17:30 -0400

    feat: escalation detector working

--- src/components/escaltionDetector/Row.jsx
+++ src/components/escaltionDetector/Row.jsx
@@ -1,27 +1,42 @@
-import { RowContainer } from "./Row.style";
-import { Input } from "./index.style";
+import { RowContainer } from './Row.style'
+import { Input } from './index.style'
 
-function Row({ handleChange, rowInfo, removeRow }) {
-  return (
-    <>
-      <RowContainer>
-        <button
-          onClick={() => removeRow(rowInfo.id)}
-          className="subtract_button"
-        >
-          -
-        </button>
-        <button className="add_button">+</button>
-        <Input
-          type="number"
-          onChange={(event) => handleChange(rowInfo.id, event)}
-          value={rowInfo.inputAmount}
-        />
-        <p className="percentage_escalation">test</p>
-        <p className="amount_escalation">test</p>
-      </RowContainer>
-    </>
-  );
+function Row({ handleChange, rowInfo, removeRow, prevValue, addRow }) {
+    // TODO: fix row displaying negative amounts if current inputAmount < prevValue
+    const percentageEscalation = (
+        (rowInfo.inputAmount / prevValue - 1) *
+        100
+    ).toFixed(3)
+
+    const amountEscalation = rowInfo.inputAmount - prevValue
+
+    return (
+        <>
+            <RowContainer>
+                <button
+                    onClick={() => removeRow(rowInfo.id)}
+                    className='subtract_button'>
+                    -
+                </button>
+                <Input
+                    type='number'
+                    onChange={(event) => handleChange(rowInfo.id, event)}
+                    value={rowInfo.inputAmount}
+                />
+                <p className='percentage_escalation'>
+                    {percentageEscalation}
+                    <span> %</span>
+                </p>
+
+                <p className='amount_escalation'>{amountEscalation}</p>
+                <button
+                    className='add_button'
+                    onClick={addRow}>
+                    +
+                </button>
+            </RowContainer>
+        </>
+    )
 }
 
-export default Row;
+export default Row
--- src/components/escaltionDetector/index.jsx
+++ src/components/escaltionDetector/index.jsx
@@ -1,85 +1,97 @@
-import { nanoid } from "nanoid";
-import { useState } from "react";
-import Row from "./Row";
+import { nanoid } from 'nanoid'
+import { useState, useEffect } from 'react'
+import Row from './Row'
 import {
-  ClearButton,
-  EscalationDetectorWrapper,
-  Input,
-  Title,
-} from "./index.style";
+    ClearButton,
+    EscalationDetectorWrapper,
+    Input,
+    Title,
+} from './index.style'
 
 function EscalationDetector() {
-  const [amountRows, setAmountRows] = useState([]);
-  const [initialInput, setInitialInput] = useState("");
+    const [amountRows, setAmountRows] = useState([])
+    const [initialInput, setInitialInput] = useState('')
 
-  const handleInitialChange = (event) => {
-    if (amountRows.length === 0) {
-      setAmountRows([
-        {
-          id: nanoid(),
-          inputAmount: "",
-          percentEscalation: "",
-          amountEscalation: "",
-        },
-      ]);
+    const handleInitialChange = (event) => {
+        if (amountRows.length === 0) {
+            setAmountRows([
+                {
+                    id: nanoid(),
+                    inputAmount: '',
+                },
+            ])
+        }
+
+        if (event.target.value === '') {
+            setAmountRows([])
+        }
+        setInitialInput(event.target.value)
     }
 
-    if (event.target.value === "") {
-      setAmountRows([]);
+    const handleChange = (id, event) => {
+        const { value } = event.target
+
+        const newRows = amountRows.map((row) => {
+            if (row.id !== id) return row
+
+            row.percentEscalation = row
+
+            return {
+                ...row,
+                inputAmount: value,
+            }
+        })
+        setAmountRows(newRows)
     }
-    setInitialInput(event.target.value);
-  };
 
-  const handleChange = (id, event) => {
-    const { value } = event.target;
+    const removeRow = (rowId) => {
+        setAmountRows((prevAmountRows) =>
+            prevAmountRows.filter((row) => row.id !== rowId)
+        )
+    }
 
-    const newRows = amountRows.map((row, index) => {
-      if (row.id !== id) return row;
+    const addRow = () => {
+        setAmountRows((prevAmountRows) =>
+            prevAmountRows.concat([
+                {
+                    id: nanoid(),
+                    inputAmount: '',
+                },
+            ])
+        )
+    }
 
-      row.percentEscalation = row;
+    const rows = amountRows.map((row, index, array) => {
+        return (
+            <Row
+                key={row.id}
+                rowInfo={row}
+                handleChange={handleChange}
+                removeRow={removeRow}
+                addRow={addRow}
+                prevValue={
+                    index == 0 ? initialInput : array[index - 1].inputAmount
+                }
+            />
+        )
+    })
 
-      return {
-        ...row,
-        inputAmount: value,
-      };
-    });
-    setAmountRows(newRows);
-  };
-
-  // {
-  //     id:
-  //     inputAmount:
-  //     percentEscalation:
-  //     amountEscalation:
-  // }
-
-  const removeRow = (rowId) => {
-    setAmountRows((prevAmountRows) =>
-      prevAmountRows.filter((row) => row.id !== rowId),
-    );
-  };
-
-  const rows = amountRows.map((row) => {
     return (
-      <Row
-        key={row.id}
-        rowInfo={row}
-        handleChange={handleChange}
-        removeRow={removeRow}
-      />
-    );
-  });
-
-  return (
-    <>
-      <EscalationDetectorWrapper>
-        <Title>Escalation Detector</Title>
-        <Input type="number" onChange={(event) => handleInitialChange(event)} />
-        <ClearButton>Clear</ClearButton>
-        {amountRows.length > 0 && rows}
-      </EscalationDetectorWrapper>
-    </>
-  );
+        <>
+            <EscalationDetectorWrapper>
+                <Title>Escalation Detector</Title>
+                <Input
+                    type='number'
+                    onChange={(event) => handleInitialChange(event)}
+                    value={initialInput}
+                />
+                <p>% Escalation</p>
+                <p>$ Escalation</p>
+                {amountRows.length > 0 && rows}
+                <ClearButton>Clear</ClearButton>
+            </EscalationDetectorWrapper>
+        </>
+    )
 }
 
-export default EscalationDetector;
+export default EscalationDetector
